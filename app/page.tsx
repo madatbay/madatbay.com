7b1f6a30f5f71ba6d4301e6da262be03f8e084fa
Madat Bayramov <medet.bayramov22@gmail.com>
2024-03-26 16:24:19 +0400

    feat: add hero-section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import BlogSection from "@/components/blog-section"
-import HeroSection from "@/components/hero-section"
+import { HeroContent, HeroSection, HeroTitle } from "@/components/hero-section"
 import SkillsSection from "@/components/skills-section"
 
 export default function Home() {
   return (
     <div className="space-y-16">
-      <HeroSection />
+      <HeroSection>
+        <HeroTitle>Software Engineer.</HeroTitle>
+        <HeroContent>
+          As a seasoned software engineer with five years of diverse experience,
+          I am currently dedicated to delivering robust solutions at a reputable
+          Swiss company. Additionally, I hold the position of CTO at a startup,
+          leveraging my expertise to drive dynamic ventures. Over the years,
+          I&apos;ve been CTO at multiple startups, broadening my entrepreneurial
+          insight. Beyond professional pursuits, I derive immense satisfaction
+          from delving into coding exploration and pursuing side projects during
+          my leisure hours.
+        </HeroContent>
+      </HeroSection>
       <SkillsSection />
       <BlogSection />
     </div>
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,17 +1,39 @@
-export default function HeroSection() {
-  return (
-    <div className="mt-8">
-      <h1 className="text-3xl font-bold">Software Engineer.</h1>
-      <p className="mt-2 max-w-2xl leading-relaxed text-muted-foreground">
-        As a seasoned software engineer with five years of diverse experience, I
-        am currently dedicated to delivering robust solutions at a reputable
-        Swiss company. Additionally, I hold the position of CTO at a startup,
-        leveraging my expertise to drive dynamic ventures. Over the years,
-        I&apos;ve been CTO at multiple startups, broadening my entrepreneurial
-        insight. Beyond professional pursuits, I derive immense satisfaction
-        from delving into coding exploration and pursuing side projects during
-        my leisure hours.
-      </p>
-    </div>
-  )
-}
+import { cn } from "@/lib/utils"
+import * as React from "react"
+
+const HeroSection = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("", className)} {...props} />
+))
+HeroSection.displayName = "HeroSection"
+
+const HeroTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h1
+    ref={ref}
+    className={cn("text-3xl font-bold leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+HeroTitle.displayName = "HeroTitle"
+
+const HeroContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "mt-2 max-w-2xl leading-relaxed text-muted-foreground",
+      className
+    )}
+    {...props}
+  />
+))
+HeroContent.displayName = "HeroContent"
+
+export { HeroContent, HeroSection, HeroTitle }
