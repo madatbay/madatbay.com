3867fd2cecd0a40ed5cac8530c86d2011fcc5886
Madat Bayramov <medet.bayramov22@gmail.com>
2024-03-26 17:04:00 +0400

    feat: add blog list page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,33 +1,24 @@
 import { HeroContent, HeroSection, HeroTitle } from "@/components/hero-section"
+import PostCard from "@/components/posts/post-card"
 import { getPosts } from "@/lib/posts"
 
 export default async function Page() {
-  const posts = await getPosts(4)
+  const posts = await getPosts()
 
   return (
-    <div>
+    <div className="space-y-8">
       <HeroSection>
-        <HeroTitle>Software Engineer.</HeroTitle>
+        <HeroTitle>Experience Spot.</HeroTitle>
         <HeroContent>
-          As a seasoned software engineer with five years of diverse experience,
-          I am currently dedicated to delivering robust solutions at a reputable
-          Swiss company. Additionally, I hold the position of CTO at a startup,
-          leveraging my expertise to drive dynamic ventures. Over the years,
-          I&apos;ve been CTO at multiple startups, broadening my entrepreneurial
-          insight. Beyond professional pursuits, I derive immense satisfaction
-          from delving into coding exploration and pursuing side projects during
-          my leisure hours.
+          Delving into my software engineering journey and technical insights in
+          my blog posts.
         </HeroContent>
       </HeroSection>
-      {posts.map((post) => (
-        <article key={post.slug}>
-          <a href={`/blog/${post.slug}`}>
-            <p>{post.date}</p>
-            <h1>{post.title}</h1>
-            <p>{post.description}</p>
-          </a>
-        </article>
-      ))}
+      <div className="grid grid-cols-1 grid-rows-[auto,auto,auto] gap-4 sm:grid-cols-2 lg:grid-cols-4">
+        {posts.map((post) => (
+          <PostCard key={post.slug} post={post} />
+        ))}
+      </div>
     </div>
   )
 }
