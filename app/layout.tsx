8ccca6b1105558272b408348ce0d3fcde489497b
Madat Bayramov <medet.bayramov22@gmail.com>
2024-03-28 02:31:39 +0400

    fix: fix meadataBase in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,7 @@ const fontMono = localFont({
 })
 
 export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.url),
   title: {
     default: siteConfig.title,
     template: `%s | ${siteConfig.title}`,
