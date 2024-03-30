1ac9db9d3592e575a38755de6c8035eed7b4f96b
Madat Bayramov <medet.bayramov22@gmail.com>
2024-03-30 18:54:46 +0400

    fix: fix sitemap date format

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,17 @@
 import { siteConfig } from "@/config/site"
 import { getPosts } from "@/lib/posts"
 
+const formatPostDate = (dateStr: string) => {
+  let date = new Date(dateStr)
+  const offset = date.getTimezoneOffset()
+  date = new Date(date.getTime() - offset * 60 * 1000)
+  return date.toISOString().split("T")[0]
+}
+
 export default async function sitemap() {
   let blogs = (await getPosts()).map((post) => ({
     url: `${siteConfig.url}/blog/${post.slug}`,
-    lastModified: post.date,
+    lastModified: formatPostDate(post.date),
   }))
 
   let routes = ["", "/blog"].map((route) => ({
