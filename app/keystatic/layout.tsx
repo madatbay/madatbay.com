d8df35ff16ca97d7e59367e7fa7e0ab0a6bf040b
Madat Bayramov <medet.bayramov22@gmail.com>
2025-10-27 12:55:45 +0400

    fix: prevent access to Keystatic app in production environment

--- app/keystatic/layout.tsx
+++ app/keystatic/layout.tsx
@@ -1,5 +1,9 @@
+import { notFound } from "next/navigation"
 import KeystaticApp from "./keystatic"
 
 export default function Layout() {
+  if (process.env.NODE_ENV !== "development") {
+    return notFound()
+  }
   return <KeystaticApp />
 }
