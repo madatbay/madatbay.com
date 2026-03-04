2c1fb60dab11ef5974772046e1c91de9650d4336
Madat Bayramov <medet.bayramov22@gmail.com>
2026-03-04 18:27:19 +0400

    feat: enhance sitemap generation by adding change frequency and TypeScript types for better structure

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,6 @@
 import { siteConfig } from "@/config/site"
 import { getCollectionEntries } from "@/lib/queries"
+import { MetadataRoute } from "next"
 
 const formatPostDate = (dateStr: string) => {
   let date = new Date(dateStr)
@@ -9,35 +10,45 @@ const formatPostDate = (dateStr: string) => {
 }
 
 export default async function sitemap() {
-  const blogs = (await getCollectionEntries("posts")).map((post) => ({
+  const blogs: MetadataRoute.Sitemap = (
+    await getCollectionEntries("posts")
+  ).map((post) => ({
     url: `${siteConfig.url}/blog/${post.slug}`,
     lastModified: formatPostDate(post.updatedAt ?? new Date().toISOString()),
+    changeFrequency: "weekly",
   }))
 
-  const caseStudies = (await getCollectionEntries("caseStudies")).map(
-    (caseStudy) => ({
-      url: `${siteConfig.url}/case-studies/${caseStudy.slug}`,
-      lastModified: formatPostDate(
-        caseStudy.updatedAt ?? new Date().toISOString()
-      ),
-    })
-  )
+  const caseStudies: MetadataRoute.Sitemap = (
+    await getCollectionEntries("caseStudies")
+  ).map((caseStudy) => ({
+    url: `${siteConfig.url}/case-studies/${caseStudy.slug}`,
+    lastModified: formatPostDate(
+      caseStudy.updatedAt ?? new Date().toISOString()
+    ),
+    changeFrequency: "monthly",
+  }))
 
-  const publications = (await getCollectionEntries("publications")).map(
-    (publication) => ({
-      url: `${siteConfig.url}/publications/${publication.slug}`,
-      lastModified: formatPostDate(
-        publication.updatedAt ?? new Date().toISOString()
-      ),
-    })
-  )
+  const publications: MetadataRoute.Sitemap = (
+    await getCollectionEntries("publications")
+  ).map((publication) => ({
+    url: `${siteConfig.url}/publications/${publication.slug}`,
+    lastModified: formatPostDate(
+      publication.updatedAt ?? new Date().toISOString()
+    ),
+    changeFrequency: "monthly",
+  }))
 
-  const routes = ["", "/blog", "/case-studies", "/publications", "/links"].map(
-    (route) => ({
-      url: `${siteConfig.url}${route}`,
-      lastModified: new Date().toISOString().split("T")[0],
-    })
-  )
+  const routes: MetadataRoute.Sitemap = [
+    "",
+    "/blog",
+    "/case-studies",
+    "/publications",
+    "/links",
+  ].map((route) => ({
+    url: `${siteConfig.url}${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+    changeFrequency: "monthly",
+  }))
 
   return [...routes, ...blogs, ...caseStudies, ...publications]
 }
